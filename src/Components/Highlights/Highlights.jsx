8d4bb806eda8707956f6d658d13f4b7290961bfb
JersonOchoa <jerson.ochoab@gmail.com>
2024-05-01 19:34:51 -0600

    [UPDATE] Store item design

--- src/Components/Highlights/Highlights.jsx
+++ src/Components/Highlights/Highlights.jsx
@@ -12,25 +12,49 @@ const Highlights = () => {
             storeId: '1',
             storeName: 'La Torre',
             storeImg: 'https://parquelasamericas.com.gt/wp-content/uploads/2018/05/La-Torre.jpg',
-            storeDescription: 'Descripción de tienda 1',
-            storeSchedule: 'Horario: Lunes a viernes 09:00 a 21:00',
+            storeDescription: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam quos praesentium, tempora voluptatem modi nemo odio.',
+            storeSchedule: '09:00 a 21:00',
+            closingSchedule: '21:00',
             storePhoneNumber: '7772-0111',
+            stars: 1,
+            categories: ['Bebidas', 'Miscelanea', 'Ferretería'],
+            delivery: ['PedidosYa',],
         },
         {
             storeId: '2',
             storeName: 'Walmart',
             storeImg: 'https://revcycleintelligence.com/images/site/features/WalmartLogo.jpg',
             storeDescription: 'Descripción de tienda 2',
-            storeSchedule: 'Horario: Lunes a viernes 09:00 a 21:00',
+            storeSchedule: '9:00 a 21:00',
+            closingSchedule: '21:00',
             storePhoneNumber: '7772-0111',
+            stars: 3.1,
+            categories: ['Bebidas', 'Miscelanea', 'Ferretería'],
+            delivery: ['PedidosYa',],
         },
         {
             storeId: '3',
             storeName: 'La Barata',
             storeImg: 'https://www.bolsadetrabajoss.com/wp-content/uploads/2019/11/LA-BARATA-EMPLEOS.png',
             storeDescription: 'Descripción de tienda 3',
-            storeSchedule: 'Horario: Lunes a viernes 09:00 a 21:00',
+            storeSchedule: '9:00 a 21:00',
+            closingSchedule: '21:00',
             storePhoneNumber: '7772-0111',
+            stars: 4,
+            categories: ['Bebidas', 'Miscelanea', 'Ferretería'],
+            delivery: ['PedidosYa',],
+        },
+        {
+            storeId: '3',
+            storeName: 'La Barata',
+            storeImg: 'https://www.bolsadetrabajoss.com/wp-content/uploads/2019/11/LA-BARATA-EMPLEOS.png',
+            storeDescription: 'Descripción de tienda 3',
+            storeSchedule: '9:00 a 21:00',
+            closingSchedule: '21:00',
+            storePhoneNumber: '7772-0111',
+            stars: 3.5,
+            categories: ['Bebidas', 'Miscelanea', 'Ferretería',],
+            delivery: ['PedidosYa',],
         },
     ]
 
@@ -41,7 +65,7 @@ const Highlights = () => {
                 {
                     highlightStoresList.map((item, index) => {
                         return(
-                            <StoreItem key={item.storeId} storeId={item.storeId} storeImg={item.storeImg} storeName={item.storeName} storeSchedule={item.storeSchedule} storePhoneNumber={item.storePhoneNumber} storeDescription={item.storeDescription}/>
+                            <StoreItem key={item.storeId} item={item} />
                         )
                     })
                 }
--- src/Components/StoreItem/StoreItem.jsx
+++ src/Components/StoreItem/StoreItem.jsx
@@ -3,21 +3,25 @@ import { useNavigate } from 'react-router-dom';
 import Rating from '@mui/material/Rating';
 import './StoreItem.css';
 
-const StoreItem = ({storeImg, storeName, storeSchedule, storeId, storePhoneNumber, storeDescription}) => {
+const StoreItem = ({item}) => {
     const navigate = useNavigate();
 
     return (
-        <div className="storeItem__item" key={storeId} onClick={() => navigate(`/store/${storeId}`)}>
-            <div className="storeItem__itemHover">
-                <h2>{storeName}</h2>
-                <h3>{storeSchedule}</h3>
-                <p><strong>{storePhoneNumber}</strong></p>
-                <p><strong>{storePhoneNumber}</strong></p>
+        <div className="storeItem__item" key={item.storeId} onClick={() => navigate(`/store/${item.storeId}`)}>
+            <img src={item.storeImg} alt="" />
+            <h4>{item.storeName}</h4>
+            {/* <p>{item.storeDescription}</p> */}
+            <p>Horario: {item.storeSchedule}</p>
+            <div className="storeItem__tags">
+                {
+                    item.categories.map((item, index) => {
+                        return (
+                            <p>{item}</p>
+                        )
+                    })
+                }
             </div>
-            <img src={storeImg} alt="" />
-            <h4>{storeName}</h4>
-            <p>{storeDescription}</p>
-            <Rating name="read-only" value={2.5} precision={0.5} readOnly />
+            <Rating name="read-only" value={item.stars} precision={0.5} readOnly />
         </div>
     )
 }
