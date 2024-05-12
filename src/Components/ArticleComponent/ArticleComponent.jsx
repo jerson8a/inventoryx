46eb3fc0e752b952f21980983278348966f0ad08
JersonOchoa <jerson.ochoab@gmail.com>
2024-05-12 16:01:23 -0600

    [UPDATE] Add to cart button functionality

--- src/Components/ArticleComponent/ArticleComponent.jsx
+++ src/Components/ArticleComponent/ArticleComponent.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { addItemToCart } from '../../features/market/marketStore';
 import './ArticleComponent.css';
+import { CartCheckFill } from 'react-bootstrap-icons';
 
 const ArticleComponent = ({item}) => {
-    const isSigned = useSelector((state) => state.isSigned.value);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const isSigned = useSelector((state) => state.isSigned.value);
+    const cart = useSelector((state) => state.marketCart.value);
+    const [isAddedToCart, setIsAddedToCart] = useState(false);
 
     const addItem = () => {
         if (isSigned) {
@@ -18,11 +21,23 @@ const ArticleComponent = ({item}) => {
         }
     }
 
+    useEffect(() => {
+        cart.filter((cartItem) => {
+            cartItem.itemId == item.itemId &&
+            setIsAddedToCart(true)
+        })
+    }, [cart])
+
     return (
         <div className="articleComponent__item">
             <div className="articleComponent__itemImg">
                 <img src={item.mainImg} alt={item.itemName} />
-                <button onClick={() => (addItem())}><strong>Añadir al carrito</strong></button>
+                {
+                    isAddedToCart ?
+                    <button className='buttonRemoveFromCart'><CartCheckFill /></button>
+                    :
+                    <button className='buttonAddToCart' onClick={() => (addItem())}><strong>Añadir al carrito</strong></button>
+                }
             </div>
             <h3>{item.itemName}</h3>
             <p>{item.currency}{parseFloat(item.price).toFixed(2)}</p>
